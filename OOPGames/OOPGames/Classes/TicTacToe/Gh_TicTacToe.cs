b3ee1cea70541ba5ab562b8fa95c5cb59c2107a5
DomiWeiser <dominic.weiser@stud.hs-kempten.de>
2021-11-15 22:21:57 +0100

    Fehler behoben

--- OOPGames/OOPGames/Classes/TicTacToe/Gh_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/Gh_TicTacToe.cs
@@ -66,7 +66,7 @@ namespace OOPGames
 
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 0; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     if (currentField[i, j] == 1)
                     {
