de04f87e947580248aa6bcb5670c79755a074b72
F-Wurm <felix.wurm@stud.hs-kempten.de>
2021-11-18 10:05:01 +0100

    Framework verändert Computerplayer implementiert

--- OOPGames/OOPGames/Interfaces/IPlayMoves.cs
+++ OOPGames/OOPGames/Interfaces/IPlayMoves.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OOPGames
 {
@@ -28,5 +29,10 @@ namespace OOPGames
     {
         //Column of the move
         int Column { get; }
+    }
+
+    public interface IKeyMove : IPlayMove
+    {
+        Key Key { get; }
     }
 }
