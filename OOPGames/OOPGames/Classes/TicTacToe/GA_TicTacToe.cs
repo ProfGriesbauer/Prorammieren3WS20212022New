7bff70387c80cc09a42fb3e98bb6fb927b6bde11
Tobias Biemel <Tobias.biemel@stud.hs-kempten.de>
2021-11-23 10:06:57 +0100

    Added previous shown Timer to GA_TicTacToe.cs after failed merge

--- OOPGames/OOPGames/Classes/TicTacToe/GA_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/GA_TicTacToe.cs
@@ -7,15 +7,29 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Reflection;
 
 namespace OOPGames
 {
     public class GA_TTTPainter : BaseTicTacToePaint
     {
+        public int _TimeLimit = 30;
+        public int _TimeLeft;
+        public long _LastChanged;
+
         public override string Name { get { return "GA_TicTacToePainter"; } }
 
         public override void PaintTicTacToeField(Canvas canvas, ITicTacToeField currentField)
         {
+            //Variable verringern um Zähler anzeigen zu lassen
+            DateTime currentTime = DateTime.Now;
+            long elapsedTicks = currentTime.Ticks - _LastChanged;
+            if (elapsedTicks > 10000000)
+            {
+                _TimeLimit--;
+                _LastChanged = currentTime.Ticks;
+            }
+
             canvas.Children.Clear();
             Color bgColor = Color.FromRgb(255, 255, 255);
             canvas.Background = new SolidColorBrush(bgColor);
@@ -44,6 +58,22 @@ namespace OOPGames
             canvas.Children.Add(l7);
             Line l8 = new Line() { X1 = 20, Y1 = 320, X2 = 320, Y2 = 320, Stroke = lineStroke, StrokeThickness = 3.0 };
             canvas.Children.Add(l8);
+
+            //Anzeige der Verbleibenden Sekunden
+            TextBlock textBlock = new TextBlock();
+            TextBlock.SetFontSize(textBlock, 20);
+            textBlock.Text = "Time Left " + _TimeLimit;
+            Color textColor = Color.FromRgb(0, 0, 0);
+            textBlock.Foreground = new SolidColorBrush(textColor);
+            Canvas.SetLeft(textBlock, 30);
+            Canvas.SetTop(textBlock, 320);
+            canvas.Children.Add(textBlock);
+            Line l9 = new Line() { X1 = 20, Y1 = 320, X2 = 20, Y2 = 350, Stroke = lineStroke, StrokeThickness = 3.0 };
+            canvas.Children.Add(l9);
+            Line l10 = new Line() { X1 = 20, Y1 = 350, X2 = 150, Y2 = 350, Stroke = lineStroke, StrokeThickness = 3.0 };
+            canvas.Children.Add(l10);
+            Line l11 = new Line() { X1 = 150, Y1 = 350, X2 = 150, Y2 = 320, Stroke = lineStroke, StrokeThickness = 3.0 };
+            canvas.Children.Add(l11);
 
             for (int i = 0; i < 3; i++)
             {
