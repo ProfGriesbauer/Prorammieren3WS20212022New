876ddc0866a0ad6eeb0eccd2287529b001fda9a3
phil161 <philipp.klausen@stud.hs-kempten.de>
2021-11-23 18:20:14 +0100

    Gruppe G StartrekKlasse erstellt, Meteo Klasse und Painterklasse angefangen

---
+++ OOPGames/OOPGames/Classes/GG_Startrek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGames
+{
+    public class GG_StartrekPainter : GG_IStartrekPainter
+    {   //Entweder oder, konnte mich nicht genug mit den Vor/Nachteilen beschäftigen
+        //List<GG_Meteo> _Meteos = new List<GG_Meteo>();
+        //GG_Meteo [] _MeteosArray;    
+    }
+
+    public class GG_Meteo : GG_IMeteo
+    {
+        private int _PositionRow;
+        private int _PositionColum;
+        public int PositionRow { get { return _PositionRow; } }
+
+        public int PositionColum { get { return _PositionColum; } }
+
+        public GG_Meteo()
+        {
+            _PositionRow = 0;
+            _PositionColum = 0;//Zufallszahl ersetzen!
+        }
+
+        public void UpdateMeteo()
+        {
+            _PositionRow++;
+        }
+    }
+}
