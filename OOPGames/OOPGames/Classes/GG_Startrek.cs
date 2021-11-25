e100b8310fdc02f8479a4c5585c0c927cbbf47a9
phil161 <philipp.klausen@stud.hs-kempten.de>
2021-11-25 15:29:17 +0100

    Zufallswert bei Meteoklasse, spawnMeteos, moveMetos in PaintGameField implementiert

--- OOPGames/OOPGames/Classes/GG_Startrek.cs
+++ OOPGames/OOPGames/Classes/GG_Startrek.cs
@@ -4,13 +4,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace OOPGames
 {
     public class GG_StartrekPainter : GG_IStartrekPainter
-    {   //Entweder oder, konnte mich nicht genug mit den Vor/Nachteilen beschäftigen
-        //List<GG_Meteo> _Meteos = new List<GG_Meteo>();
-        //GG_Meteo [] _MeteosArray;    
+    {  
+        List<GG_Meteo> _Meteos = new List<GG_Meteo>();  
+        uint _aufrufe = 0;
+        int _spawnspeed = 20; //Takt der Meteoerzeugung
+        int _spawnnum = 2; //Anzahl der je Spawn erzeugten Meteos
+        int _movespeed = 20; //Takt der Bewegung 
+        bool _collision = false;
+
+
+        bool GG_IStartrekPainter.Collison { get { return _collision; } }
+
+        public string Name => throw new NotImplementedException();
+
+        public void checkCollison()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void spawnMeteos()
+        {
+            GG_Meteo meteo = new GG_Meteo();
+            //Gegen Meteos an gleicher Posititon absichern
+            while (meteo.PositionColum == _Meteos[_Meteos.Count].PositionColum)
+            {
+                meteo = new GG_Meteo();
+            }
+            _Meteos.Add(meteo);
+        }
+
+        public void moveMetos()
+        {
+            foreach (GG_Meteo meteo in _Meteos)
+            {
+                meteo.UpdatePos();
+                //Wenn Meteo aus Spielfeld -> Löschen
+                if(meteo.PositionRow > 6)
+                {
+                    _Meteos.Remove(meteo);
+                }
+            }
+        }
+
+        public void PaintGameField(Canvas canvas, IGameField currentField)
+        {
+            _aufrufe++;
+
+            if((_aufrufe+_spawnspeed)%_spawnspeed == 0) //+spwanspeed dass bei Spielstart gleich gespawnd wird
+            {   for(int i = 0; i<_spawnnum; i++)
+                {
+                    spawnMeteos();
+                }
+                
+            }
+            if(_aufrufe%_movespeed == 0)
+            {
+                moveMetos();
+            }
+
+        }
     }
 
     public class GG_Meteo : GG_IMeteo
@@ -22,9 +79,11 @@ namespace OOPGames
         public int PositionColum { get { return _PositionColum; } }
 
         public GG_Meteo()
-        {
+        {   //Bei erstellen des Objekts wird der Meteo in einer zufälligen Spalte in Reihe
+            //0 gesetzt
+            Random rand = new Random();
             _PositionRow = 0;
-            _PositionColum = 0;//Zufallszahl ersetzen!
+            _PositionColum = rand.Next(0,5);
         }
 
         public void UpdatePos()
--- OOPGames/OOPGames/Interfaces/GG_IStartrek.cs
+++ OOPGames/OOPGames/Interfaces/GG_IStartrek.cs
@@ -11,8 +11,8 @@ namespace OOPGames
     //Überprüft Kollisionen
     //Zeichnet Spielfeld
     //Methode _CurrentPainter.PaintGameField() wird alle 40ms abgerufen (MainWindow.xaml.cs Zeile 97)
-    public interface GG_IStartrekPainter
-    {
+    public interface GG_IStartrekPainter : IPaintGame
+    {   bool Collison { get; }
         //ToDo:
         //-Erbung von entsprechenden Framework-Interfaces
         //-Implementierung der IPaintGame Methoden
@@ -20,14 +20,22 @@ namespace OOPGames
         //für neue Meteoritenerzeugung und das Weiterbewegen der Meteoriten bestimmt werden
 
         //PaintGameField muss der reihenach:
-        //-Spielfeld zeichnen (Matrix auswerten, entsprechende Symbole setzen)
+        //-Spielfeld zeichnen (Matrix auswerten, entsprechende Symbole setzen), siehe IPaintGame
+        //--> PaintGameField-Methode, in dieser dann die folgenden Methoden bei bestimmter Anzahl
+        //von Aufrufen, aufrufen:
         //-nach bestimmten Rythmus neue Meteoobjekte erzeugen und in einer Liste gehalten werden 
+        void spawnMeteos();
+
         //-nach bestimmten Rythmus alle Meteoobjekte aktuallisieren
+        void moveMetos();
+
         //-bei allen Meteoobjekten die Position abrufen und entsprechend im Spielfeld eintragen
         //  wenn Meteorit das Spielfeld damit verlässt aus Liste löschen https://www.csharp-examples.net/foreach/
         //-auf Kollision prüfen und im Fall von Kollision entsprechende Bildschirmausgabe erzeugen 
+        void checkCollison();
         //  Kollison muss dann den Wert von CheckifPlayerWon in der Rulesklasse auf true setzen, dann wurd nach Playerinteraktion(Mausklick)
         // das Spiel wirklich beendet und die Anzeige entsprechend gestalltet
+
     }
 
 
@@ -40,7 +48,7 @@ namespace OOPGames
     public interface GG_IStartrekRules : IGameRules
     {
         //Hält Gamefield!
-        //Muss bei Painter abfragen, ob Kollision vorliegt
+        //Muss bei Painter abfragen, ob Kollision vorliegt --> Collsion-Variable
 
     }
 
