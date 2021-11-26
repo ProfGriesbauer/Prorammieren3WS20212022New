3ad3296c3ef26d6d83afc076379b2aa53d04b2b4
Michael Holzmann <michaelholzmann7@gmail.com>
2021-11-26 10:28:52 +0100

    GC_Base4Gewinnt.cs und GC_I4Gewinnt erstellt

---
+++ OOPGames/OOPGames/Classes/4Gewinnt/GC_Base4Gewinnt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGames.Classes._4Gewinnt
+{
+    class GC_Base4Gewinnt
+    {
+    }
+}
