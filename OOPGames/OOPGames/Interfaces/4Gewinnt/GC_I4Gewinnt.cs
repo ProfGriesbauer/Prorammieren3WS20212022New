22e72a951c20a43a9804007855e5b72846abdd69
Michael Holzmann <michaelholzmann7@gmail.com>
2021-11-26 11:11:31 +0100

    Dateien auf Basis der TicTactoe-Programme programmiert

--- OOPGames/OOPGames/Interfaces/4Gewinnt/GC_I4Gewinnt.cs
+++ OOPGames/OOPGames/Interfaces/4Gewinnt/GC_I4Gewinnt.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
-namespace OOPGames.Interfaces._4Gewinnt
+namespace OOPGames
 {
-    class GC_I4Gewinnt
+  
+    public interface IPaintVierGewinnt : IPaintGame
     {
+        void PaintVierGewinntField(Canvas canvas, IVierGewinntField currentField);
+    }
+
+
+
+    public interface IVierGewinntField : IGameField
+    {
+        int this[int r, int c] { get; set; }
+    }
+
+
+
+    public interface IVierGewinntRules : IGameRules
+    {
+        IVierGewinntField VierGewinntField { get; }
+        void DoVierGewinntMove(IVierGewinntMove move);
+    }
+
+
+
+    public interface IVierGewinntRules_GE : IVierGewinntRules
+    {
+        void AskForGameSize();
+    }
+
+ 
+
+    public interface IVierGewinntMove : IRowMove, IColumnMove
+    {
+
+    }
+
+
+
+    public interface IHumanVierGewinntPlayer : IHumanGamePlayer
+    {
+        IVierGewinntMove GetMove(IMoveSelection selection, IVierGewinntField field);
+    }
+
+
+
+    public interface IComputerVierGewinntPlayer : IComputerGamePlayer
+    {
+        IVierGewinntMove GetMove(IVierGewinntField field);
     }
 }
