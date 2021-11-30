c348877e59a2ba24c886a9d02671a5c5300b0fb6
F-Wurm <felix.wurm@stud.hs-kempten.de>
2021-11-30 17:43:01 +0100

    Gruppe B Labyrint Rahmen-Interface hinzugefügt

---
+++ OOPGames/OOPGames/Classes/Labyrinth/Labyrinth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OOPGames.Classes.Labyrinth
+{
+    public class PaintLabyrinth
+    {
+        
+    }
+
+    public class LabyrinthField
+    {
+        
+    }
+
+    public class LabyrinthRules
+    {
+        
+    }
+
+    public class LabyrinthMove
+    {
+
+    }
+
+    public class HumanLabyrinthPlayer
+    {
+        
+    }
+
+    public class ComputerLabyrinthPlayer
+    {
+        
+    }
+}
---
+++ OOPGames/OOPGames/Interfaces/Labyrint/ILabyrinth.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OOPGames.Interfaces.Labyrint
+{
+    public interface IPaintLabyrinth : IPaintGame
+    {
+        void PaintLabyrinthField(Canvas canvas, ILabyrinthField labyrinthField);
+    }
+
+    public interface ILabyrinthField : IGameField
+    {
+        int this[int r, int c] { get; set; }
+    }
+
+    public interface ILabyrinthRules : IGameRules
+    {
+        ILabyrinthField _LabyrinthField { get; }
+
+        void DoLabyrinthMove(ILabyrinthMove labyrinthMove);
+    }
+
+    public interface ILabyrinthMove : IRowMove, IColumnMove 
+    { 
+
+    }
+
+    public interface IHumanLabyrinthPlayer : IHumanGamePlayer
+    {
+        ILabyrinthMove GetMove(ILabyrinthMove labyrinthMove);
+    }
+
+    public interface IComputerLabyrinthPlayer : IComputerGamePlayer
+    {
+        ILabyrinthMove GetMove(ILabyrinthMove labyrinthMove);
+    }
+}
